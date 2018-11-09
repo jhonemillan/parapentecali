8d85d1468e8cf6d8a918496f0eac0a796d12e8d4
Jhon <jhonemillan@gmail.com>
2018-11-09 00:45:10 -0500

    first commit

---
+++ server/models/clientes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(Clientes) {
+
+let re = /^(([^<>()[\]\\.,;:\s@\"]-(\.[^<>()[\]\\.,;:\s@\"]-)*)|(\".-\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]-\.)-[a-zA-Z]{2,}))$/;
+
+Clientes.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
+
+};
---
+++ server/models/piloto.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(Piloto) {
+
+    let re = /^(([^<>()[\]\\.,;:\s@\"]-(\.[^<>()[\]\\.,;:\s@\"]-)*)|(\".-\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]-\.)-[a-zA-Z]{2,}))$/;
+
+    Piloto.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
+};
