05863ba82bbf380fdb5416add622a5270824c492
Jhon <jhonemillan@gmail.com>
2018-11-28 18:27:45 -0500

    fix form

--- client/bookingfly/src/app/services/client.service.ts
+++ client/bookingfly/src/app/services/client.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
-import { map, retry } from 'rxjs/operators';
+import { map, retry, switchMap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { InfoMedica } from '../models/info-medica';
 
@@ -20,16 +20,10 @@ export class ClientService {
     return this.http.post<any>(environment.url + '/clientes', cliente)
     .pipe(
       retry(2),
-      map(cliente => {
-        this.http.post<any>(environment.url + '/clientes/' + 
-                            cliente.id+'/enfermedades/', infomedica)
-                            .pipe(
-                              map(
-                                data=>{
-                                  return {cliente, data}
-                                }
-                              )
-                            )
+      switchMap(newclient => {
+        console.log(newclient);
+        return this.http.post<any>(environment.url + '/clientes/' + 
+                            newclient.id +'/enfermedades', infomedica);                      
         
       })
     );
